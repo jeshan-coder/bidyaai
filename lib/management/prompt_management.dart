4c6e1e8c42cdc6d22e2023792655e399f185a8a9
jeshan <jeshanpokharel123@gmail.com>
2025-07-25 08:01:43 +0545

    quiz feature initial commit

---
+++ lib/management/prompt_management.dart
@@ -0,0 +1,14 @@
+class PromptManager
+{
+
+//   for general chat messages
+static String generateChatPrompt(String userMessage)
+{
+  return userMessage;
+}
+
+static String generateGuizRequestPrompt(String topic)
+
+
+
+}
